1a8f6b3af98e0e5a2546ffa12c2d069ed3f1762a
Malthe Elkær <dirako@gmail.com>
2013-02-10 02:52:09 +0100

    Initial commit
    Copy button doesn't copy to clipboard yet, otherwise funcitonal

--- Nyaanimu/AppDelegate.h
+++ Nyaanimu/AppDelegate.h
@@ -11,5 +11,14 @@
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *tfTitle;
+@property (weak) IBOutlet NSTextField *tfGroup;
+@property (weak) IBOutlet NSPopUpButton *pbResolution;
+@property (weak) IBOutlet NSPopUpButton *pbSource;
+@property (weak) IBOutlet NSButton *bGenerate;
+- (IBAction)generate:(id)sender;
+@property (weak) IBOutlet NSTextField *tfRSS;
+@property (weak) IBOutlet NSButton *bCopy;
+- (IBAction)copy:(id)sender;
 
 @end
--- Nyaanimu/AppDelegate.m
+++ Nyaanimu/AppDelegate.m
@@ -15,4 +15,19 @@
     // Insert code here to initialize your application
 }
 
+- (IBAction)generate:(id)sender {
+    NSString *title = [_tfTitle stringValue];
+    NSString *group = [_tfGroup stringValue];
+    NSString *resolution = [_pbResolution titleOfSelectedItem];
+    if ([resolution isEqual: @"720p"])
+        resolution = @"-480p+-1080p+-XviD";
+    NSString *source = [_pbSource titleOfSelectedItem];
+    if ([source isEqual: @"TV"])
+        source = @"-BD";
+    NSString *init = @"http://www.nyaa.eu/?page=rss&term=";
+    _tfRSS.stringValue = [NSString stringWithFormat:@"%@%@+%@+%@+%@",init, title, group,resolution, source];
+    
+}
+- (IBAction)copy:(id)sender {
+}
 @end
